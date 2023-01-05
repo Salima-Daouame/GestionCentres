17dc7bd17f4b040c4ed89ab93eee1f4bfa319097
salima-Daouame <salima.daouame@edu.uiz.ac.ma>
2023-01-05 07:56:32 +0100

    first commit

---
+++ app/src/main/java/com/example/gestioncentres/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.gestioncentres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_main);
+
+        // How to show activity sometimes second  and go to another activity
+        Handler handler = new Handler() ;
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent inten1 = new Intent(MainActivity.this, login_compte.class);
+                        startActivity(inten1);
+                    }
+                });
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 2000);//Wait 2 seconds then run the timer
+    }
+}
